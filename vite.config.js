c6f90520362826af4c064664d7c5a7010dc1a0dd
bakare mopelola <olorimaikudi@gmail.com>
2024-07-14 08:05:37 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://api.timbu.cloud',
+      "/proxy": {
+        target: "https://timbu.cloud",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/proxy/, '')
       },
     },
-  },
-});
+  }
+})
