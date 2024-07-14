f96a56b0c3a4b5cb970ca6962707aaef39177491
bakare mopelola <olorimaikudi@gmail.com>
2024-07-14 07:09:45 +0100

    correction

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import CartProvider from './Context/CartContext.jsx'; // Ensure the correct path and default export
+import CartProvider from './Context/CartContext.jsx';   
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
